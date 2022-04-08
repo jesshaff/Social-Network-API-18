5dbe0c5f6adbc13a7e4804e2d13cb29318dc77c9
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2022-04-07 20:23:46 -0400

    trying to fix error with getting single user

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,8 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/ourspace';
-
-connect(connectionString, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ourspace', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
-module.exports = connection;
+module.exports = mongoose.connection;
--- models/user.js
+++ models/user.js
@@ -1,43 +1,44 @@
 const { Schema, model } = require('mongoose');
 
-// Schema to create User model
 const userSchema = new Schema(
   {
     username: {
       type: String,
-      unique: true,
       required: true,
+      unique: true,
       trim: true,
-      max_length: 50,
     },
     email: {
       type: String,
       required: true,
       unique: true,
-      match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\. [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please add a valid email address.',]
+      match: [/.+@.+\..+/, 'Must match an email address!'],
     },
-    thoughts: [{
-      type: Schema.Types.ObjectId,
-      ref: 'Thought'
-    }],
-    friends: [{
-      type: Schema.Types.ObjectId,
-      ref: 'User'
-    }],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: {
       virtuals: true,
-      getters: true,
     },
     id: false,
   }
 );
 
-userSchema.virtual('friendCount').get(function() {
+userSchema.virtual('friendCount').get(function () {
   return this.friends.length;
 });
-const User = model('user', userSchema);
+
+const User = model('User', userSchema);
 
 module.exports = User;
